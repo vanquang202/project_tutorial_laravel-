5d3c7023d6c9107e30d81aad7c17a1242b3c4ab5
root <root@LAPTOP-JDSULJ8S.localdomain>
2022-07-16 23:00:25 +0700

    Commit

---
+++ app/Services/Traits/Crub.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Services\Traits;
+
+use Illuminate\Support\Facades\Request;
+
+trait Crub{
+
+     public function index()
+    {
+        return view($this->views['list'], $this->getData());
+    }
+
+    public function create()
+    {
+        return view($this->views['create'], $this->getDataCreate());
+    }
+
+    public function store(Request $request)
+    {
+        $this->data::create($request->all());
+
+        return redirect($this->views['list']);
+    }
+
+    public function edit(Request $request, $id)
+    {
+        return view($this->views['edit'], $this->getDataEdit($id));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->data::find($id)->update($request->all());
+
+        return redirect($this->views['list']);
+    }
+
+    public function destroy($id)
+    {
+        $this->data::find($id)->delete();
+
+        return redirect($this->views);
+    }
+}
