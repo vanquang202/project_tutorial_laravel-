f7e11eba054db4bd786f513199d385933b3362c7
nguyenvantrong25112002 <nguyenvantrong25112002@gamil.com>
2022-07-23 09:22:57 +0700

    up

--- database/migrations/2022_07_23_020326_create_category_table.php
+++ database/migrations/2022_07_23_020326_create_category_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateCategoryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('test', function (Blueprint $table) {
-            $table->id();
+        Schema::create('categorys', function (Blueprint $table) {
+            $table->id('id');
             $table->string('name');
-            $table->string('image');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('test');
+        Schema::dropIfExists('category');
     }
-};
+}
