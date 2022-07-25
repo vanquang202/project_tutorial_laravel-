6a1bb21cc9e5dd1871b6d918f0f527f976573edf
nguyenvantrong25112002 <nguyenvantrong25112002@gamil.com>
2022-07-25 23:44:17 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Web\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,13 +19,25 @@ Route::get('/', function () {
     echo 'Login';
 });
 
-Route::get('login',[AuthController::class , 'login']);
-Route::get('redirect', [AuthController::class , 'redirect'])->name('login.redirect');
-
-
-
-Route::get('callback', [AuthController::class , 'callback'])->name('login.callback');
+Route::get('login', [AuthController::class, 'login']);
+Route::get('redirect', [AuthController::class, 'redirect'])->name('login.redirect');
+Route::get('callback', [AuthController::class, 'callback'])->name('login.callback');
 
 // Route::get('/create',function () {})->name('create');
 // Route::get('/update/{id}',function () {})->name('update');
 // Route::delete('/delete/{id}',function () {})->name('delete');
+Route::prefix('admin')->group(function () {
+    Route::get('', [DashboardController::class, 'index']);
+    Route::prefix('course')->group(function () {
+    });
+    Route::prefix('category')->group(function () {
+    });
+    Route::prefix('classroom')->group(function () {
+    });
+    Route::prefix('student')->group(function () {
+    });
+    Route::prefix('calendar')->group(function () {
+    });
+    Route::prefix('voucher')->group(function () {
+    });
+});
